f5831ec88602948960b24a927cff9cef0c8c41fc
jonibek99 <jonibekannaev5@gmail.com>
2025-04-30 14:01:50 +0500

    me4

--- basic_function_parameter04.py
+++ basic_function_parameter04.py
@@ -2,3 +2,6 @@
 # Inside the function, calculate the average of all the numbers in the given list.
 # Return the average.
 # Return the average.
+def calculate_average(a):
+    return sum(a)/len(a)
+print(calculate_average([44,653,23542,34568,4321]))
