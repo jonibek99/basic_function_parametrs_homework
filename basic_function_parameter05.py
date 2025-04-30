bc59fedb0f8d4a9081e7d253b31417b42253e4bb
jonibek99 <jonibekannaev5@gmail.com>
2025-04-30 14:07:48 +0500

    me5

--- basic_function_parameter05.py
+++ basic_function_parameter05.py
@@ -1,3 +1,11 @@
 # Create a function named count_vowels that takes a string as a parameter.
 # Inside the function, count the number of vowels (a, e, i, o, u, A, E, I, O, U) in the given string.
 # Return the count of vowels.
+def count_vowels(a):
+    c=0
+    for i in a:
+        if i in ('a, e, i, o, u, A, E, I, O, U'):
+            c+=1
+    return c
+f=str(input())
+print(count_vowels(f))
